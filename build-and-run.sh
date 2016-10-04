ff89ec2f128a2a75a9565b5ea4d9b8586f5f3d02
hinst <hinstance@yandex.ru>
2016-10-04 17:49:31 +0300

    build script improved

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,5 +1,12 @@
 dir=$(pwd)
-go build github.com/jteeuwen/go-bindata/go-bindata \
-&& ./go-bindata -o src/perfmongo_app/resources.go src/page \
+if [ ! -f go-bindata ]
+then
+	go build github.com/jteeuwen/go-bindata/go-bindata
+	if [[ $? != 0 ]]
+	then
+		exit
+	fi
+fi
+./go-bindata -o src/perfmongo_app/resources.go src/page \
 && go build perfmongo_app \
 && ./perfmongo_app
